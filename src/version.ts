b60ae5b5214c85ac7506b5147e4260b4e8dc7593
Jinghui Hu <hujinghui@buaa.edu.cn>
2026-07-11 16:22:17 +0800

    build: publish dist only, disable source/declaration maps

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
 export const APP_NAME = '@jeansoft/cocos-icon-gen';
-export const APP_VERSION = '0.2.6';
+export const APP_VERSION = '0.2.7';
 export const APP_DESCRIPTION = 'Generate app icons for Cocos projects';

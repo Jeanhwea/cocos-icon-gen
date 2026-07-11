819ee5d721ad9eca849f5bc06cacc4bd928dd826
Jinghui Hu <hujinghui@buaa.edu.cn>
2026-07-11 13:21:40 +0800

    Revert "chore: sync version in version.ts with package.json (0.1.6)"
    This reverts commit 0d885ae47a58455356c22ee93392670bee92b77e.

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
 export const APP_NAME = 'cocos-icon-gen';
-export const APP_VERSION = '0.1.6';
+export const APP_VERSION = '0.1.0';
 export const APP_DESCRIPTION = 'Generate app icons for Cocos projects';
